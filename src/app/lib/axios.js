46a75a4c0967f0bbd69ed7d49c65ce09386b848a
lloydestrada <iamlloydestrada@gmail.com>
2025-09-09 16:49:08 +0800

    refactor for sign up/in features

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,11 +1,18 @@
-// src/app/lib/axios.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // token from login
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // attach token
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
